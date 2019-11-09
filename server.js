47a9f4ab13769ce8c758d3df648734d1dd43bbcc
shiv <shivprataprai11@gmail.com>
2019-11-09 19:33:31 +0530

    server file added

---
+++ server.js
@@ -0,0 +1,9 @@
+
+const express = require('express')
+const app = express()
+function uzzu(req,res)
+{
+  res.send("<h1>hellow world</h1>");
+}
+app.get('/',uzzu)
+app.listen(80,()=>console.log("server started"))
